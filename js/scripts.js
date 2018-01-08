bd90aafbbb701acbe976f51b4e7ca27272306bb5
acolautti <acolautti@santafe.gov.ar>
2018-01-08 13:40:59 -0300

    sdfas
    sadsfas

--- js/scripts.js
+++ js/scripts.js
@@ -19,30 +19,30 @@
     // ---------------------------------------------- 
     //  magnific-popup
     // ----------------------------------------------
-	(function () {
-
-		$('.portfolio-items').magnificPopup({ 
-			delegate: 'a',
-			type: 'image',
-			// other options
-			closeOnContentClick: false,
-			closeBtnInside: false,
-			mainClass: 'mfp-with-zoom mfp-img-mobile',
-
-			gallery: {
-				enabled: false
-			},
-			zoom: {
-				enabled: true,
-				duration: 300, // don't foget to change the duration also in CSS
-				opener: function(element) {
-					return element.find('i');
-				}
-			}
-
-		});
-
-	}()); 
+    // (function () {
+    //
+		// $('.portfolio-items').magnificPopup({
+		// 	delegate: 'a',
+		// 	type: 'image',
+		// 	// other options
+		// 	closeOnContentClick: false,
+		// 	closeBtnInside: false,
+		// 	mainClass: 'mfp-with-zoom mfp-img-mobile',
+    //
+		// 	gallery: {
+		// 		enabled: false
+		// 	},
+		// 	zoom: {
+		// 		enabled: true,
+		// 		duration: 300, // don't foget to change the duration also in CSS
+		// 		opener: function(element) {
+		// 			return element.find('i');
+		// 		}
+		// 	}
+    //
+		// });
+    //
+    // }());
 
 
 
